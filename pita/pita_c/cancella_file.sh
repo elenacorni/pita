dbb68471dec179f6ed3168f9bf83f7bd84a71b78
elenacorni <elena.corni@cnaf.infn.it>
2017-02-28 09:38:45 +0100

    rm

--- pita/pita_c/cancella_file.sh
+++
@@ -1,4 +0,0 @@
-rm tmp_*
-rm file_output_da_mod_column.txt
-rm core.*
-rm *_ext_utr.stab
--- pita/pita_c/compile_and_run.sh
+++
@@ -1,2 +0,0 @@
-g++ -std=c++11 lib/cRNA.cpp -o lib/XXXX
-perl pita_prediction.pl -utr input_utr.fa -mir input_microRNAs.fa -prefix $1
